cdd9e57d9fa4d7d9bd56ba57329996121a515bfe
monju <monjurmorshed793@gmail.com>
2020-11-27 15:30:17 +0600

    initial project set up

---
+++ src/test/java/com/monjur/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monjur.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
